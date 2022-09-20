569ee82e890a371cd630ca4810147fc6ccf9dae1
gradendripar <1318247793@qq.com>
2022-09-20 14:19:01 +0800

    "第三次提交"

--- stfui/src/main.js
+++ stfui/src/main.js
@@ -3,12 +3,18 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
+import {deleteRequest, getRequest, postRequest, putRequest} from "./api/login";
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
 
+Vue.prototype.postRequest = postRequest;
+Vue.prototype.getRequest = getRequest;
+Vue.prototype.putRequest = putRequest;
+Vue.prototype.deleteRequest = deleteRequest;
+
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
